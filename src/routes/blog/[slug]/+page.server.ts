0840abfc13444bfa8e9f7737d99cc807faf876bd
janibehm <jani.behm@gmail.com>
2025-04-24 00:04:36 +0300

    Disable Prerendering for Blog Posts

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,17 +1,7 @@
 import { error } from '@sveltejs/kit';
-import { getPosts, getPost, type Post } from '$lib/sanity';
+import { getPost } from '$lib/sanity';
 
-export const prerender = true;
-
-// This function tells SvelteKit which slugs to prerender
-export async function entries() {
-  const posts = await getPosts();
-  
-  // Return an array of slug objects for each post
-  return posts.map((post: Post) => ({
-    slug: post.slug.current
-  }));
-}
+export const prerender = false; // Disable prerendering for this route
 
 export async function load({ params }) {
   const { slug } = params;
